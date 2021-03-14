ab9797bb8f68fe3ac3d426f0312c21ebe4438322
Diecaal <diecaal@gmail.com>
2021-03-14 19:18:12 +0100

    Integration Solid POD with Map Visualization

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
-import Welcome from './components/Welcome';
-import EmailForm from "./components/EmailForm";
-import UserList from "./components/UserList";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Map from "./components/Map";
 
-class App extends React.Component{
-  constructor(props){
+import './App.css';
+const auth = require('solid-auth-client');
+const { default: data } = require('@solid/query-ldflex');
+
+class App extends React.Component {
+  constructor(props) {
     super(props)
     this.getLocation();
     this.state = {
-      users:[],
-      currentLat : null,
-      currentLng : null
+      users: [],
+      currentLat: null,
+      currentLng: null
     };
   }
-
   refreshUsers(users){
-    this.setState({users:users})
+    this.setState({ users: users })
   }
   
   getLocation() {
     const self = this;
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) { //watchPosition()
+      navigator.geolocation.getCurrentPosition(function (position) { //watchPosition()
         self.setState({
           currentLat: position.coords.latitude,
           currentLng: position.coords.longitude
@@ -35,28 +33,163 @@ class App extends React.Component{
   }
   
   render(){
-    return(
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <Welcome name="ASW students"/>
-        </header>
+        <div className="App">
+          <header>
+            <h1>Admin Radain</h1>
+          </header>
+          <LocationsList />
+        </div>
         <div className="App-content">
           {
             this.state.currentLat && this.state.currentLng ?
-            <Map lat={this.state.currentLat} lng={this.state.currentLng}/>
-            : <h2>Location needed for services</h2>
+              <Map lat={this.state.currentLat} lng={this.state.currentLng} />
+              : <h2>Location needed for services</h2>
           }
-          <EmailForm refreshUsers={this.refreshUsers.bind(this)}/>
-          <UserList users={this.state.users}/>
-          <a className="App-link"
-            href="https://github.com/pglez82/radarin_0"
-            target="_blank"
-            rel="noopener noreferrer">Source code</a>
         </div>
       </div>
     )
   }
+
 }
+
+class LocationsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: [],
+    }
+  }
+
+  handleNewLocation(location) {
+    if (location === "") {
+      alert("Empty location is not allowed!");
+      return;
+    }
+    const locations = this.state.locations.concat(location);
+    this.setState({ locations });
+  }
+
+  handleDeleteLocation(location) {
+    const locations = this.state.locations.slice();
+    locations.splice(locations.indexOf(location), 1);
+    this.setState({ locations });
+  }
+
+  async loadFromSolid() {
+    let locations = await loadSolidLocations();
+    this.setState({ locations });
+  }
+
+  async saveToSolid() {
+    let oldLocations = await loadSolidLocations();
+    saveSolidLocations(this.state.locations, oldLocations);
+  }
+
+  render() {
+    return (
+      <div className="App-content">
+        <InputLocation addNewLocation={(location) => this.handleNewLocation(location)} />
+        <LocationList locations={this.state.locations} deleteLocation={(location) => this.handleDeleteLocation(location)} />
+        <SolidStorage loadFromSolid={() => this.loadFromSolid()} saveToSolid={() => this.saveToSolid()} />
+      </div>
+    );
+  }
+}
+
+class InputLocation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.props.addNewLocation(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="location" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="+" />
+      </form>
+    );
+  }
+
+}
+
+//TODO: MODIFICAR ESTO PARA RENDERIZAR EL MAPA (API GOOGLE MAPS)
+function LocationList(props) {
+  var data =
+    props.locations.map(l => { // l es cada location
+      var tp = l.split(',');
+      var d = {
+        lat: tp[0],
+        lon: tp[1]
+
+      }
+      return d;
+    })
+  console.log(JSON.stringify(data));
+  return null;
+}
+
+class SolidStorage extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.props.loadFromSolid()}>Load from Solid</button>
+        <button onClick={() => this.props.saveToSolid()}>Save to Solid</button>
+      </div>
+    );
+  }
+}
+
+async function loadSolidLocations() {
+  let session = await getCurrentSession();
+  let url = session.webId.replace("profile/card#me", "radarin/last.ttl#locations");
+  let locationList = data[url];
+  const locations = [];
+  for await (const location of locationList.schema_itemListElement) {
+    locations.push(location.toString());
+  }
+  return Array.from(locations.values());
+}
+
+async function saveSolidLocations(newlocations, oldLocations) {
+  let session = await getCurrentSession();
+  let url = session.webId.replace("profile/card#me", "radarin/last.ttl#locations");
+  let locations = data[url];
+  for (const t of oldLocations) {
+    await locations["schema:itemListElement"].delete(t.toString());
+  }
+  for (const t of newlocations) {
+    await locations["schema:itemListElement"].add(t.toString());
+  }
+  alert("Saved to your Solid POD");
+}
+
+async function getCurrentSession() {
+  let session = await auth.currentSession();
+  let popupUri = 'https://inrupt.net/common/popup.html';
+  if (!session) {
+    session = await auth.popupLogin({ popupUri });
+  }
+  return session;
+}
+
+//MAPA
+
 
 export default App;
