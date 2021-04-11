017e410d580df2c7022fbd174b49f7299120d6d4
Diecaal <diecaal@gmail.com>
2021-04-11 17:51:54 +0200

    Friends profile pic displayed + Map Component comments

--- webapp/src/components/Map.js
+++ webapp/src/components/Map.js
@@ -8,53 +8,80 @@ import {
 } from "@react-google-maps/api";
 import mapsStyles from "./MapStyles";
 
+//Libraries to be used by GoogleMaps API
 const libraries = ["places","geometry"];
 const mapContainerStyle = {
     width: '68vw',
     height: '68vh',
 }
+
+// UI Options for displayed map
 const options = {
     styles: mapsStyles,
     disableDefaultUI: true,
     zoomControl: true,
 }
 
-const Map = ( {lat,lng,locations,range,friends} ) => {    
+/**
+ * Constant storing resulting Map configuration and its behaviour
+ */
+const Map = ( {lat,lng,locations,range,friendsNames,friendsPhotos} ) => {    
     const [selected, setSelected] = React.useState(null);
     const markers = [];
     
+    /**
+     * Load GoogleMaps API key and used libraries
+     */
     const {isLoaded,loadError} = useLoadScript({
         googleMapsApiKey : process.env.REACT_APP_GOOGLE_KEY,
         libraries,
     });
 
+    /**
+     * Shows distance between user current location and a target location
+     * @param {number} lat2 latitude of target location
+     * @param {number} lng2 longitude of target location
+     * @returns {number} distance to target location 
+     */
     function distanceBetweenCoordinates(lat2, lng2){
         return window.google.maps.geometry.spherical.computeDistanceBetween(new window.google.maps.LatLng({lat: lat,lng: lng}),
             new window.google.maps.LatLng({lat:lat2,lng:lng2}));
     }
 
+    // Prompts if error detected while loading map
     if(loadError) return "Error loading map";
     if(!isLoaded) return "Map not loaded";
 
-    // Map associating locations with user's last location
-    const friendsMapLocations = (locations, friends) => {
+    /**
+     * Map associating locations with user's last location, their names and profile photos
+     * @param {String[]} locations 
+     * @param {String[]} friendsNames 
+     * @param {String[]} friendsPhotos 
+     * @returns {Map<String,String[]>}
+     */
+    const friendsMapLocations = (locations, friendsNames, friendsPhotos) => {
         const map = new Map();
-        for(let i=0;i<locations.length;i++)
-            map.set(locations[i],friends[i]);
+        for(let i=0;i<locations.length;i++) {
+            map.set(locations[i],[ friendsNames[i],friendsPhotos[i] ]);
+        }
         return map;
     }
-    const friendsLocations = friendsMapLocations;
 
-    // Turn string locations into google markers
+    /**
+     * Map storing friends locations ( Key: userLocation, Values: [userName,userPhoto] )
+     */
+    const friendsLocations = friendsMapLocations; 
+
+    // Turn string locations into google markers objects
     locations.map((location) => markers.push(<Marker 
         key={friendsLocations.get(location)} 
         position={{
             lat: parseFloat(location.split(",")[0]), 
             lng: parseFloat(location.split(",")[1])
         }}
-        icon={{
-            url: '/user.png', 
-            scaledSize: new window.google.maps.Size(15,15)
+        icon={{ // If user has a profile image we select it, otherwise we user a default one
+            url: friendsLocations.get(selected)[1] === undefined ? "/user.png" : friendsLocations.get(selected)[1], 
+            scaledSize: new window.google.maps.Size(20,20)
         }}
         onClick={() => setSelected(location)}
     />))
@@ -66,6 +93,7 @@ const Map = ( {lat,lng,locations,range,friends} ) => {
             zoom={12} 
             center={{lat:lat, lng:lng}}
             options={options}>
+            {/* User current location marker */}
             <Marker 
                 position={{lat:lat, lng:lng,}} 
                 icon={{
@@ -73,13 +101,17 @@ const Map = ( {lat,lng,locations,range,friends} ) => {
                     scaledSize: new window.google.maps.Size(36,36) 
                 }}
             />
+            {/* Visualization of range selected by the user */}
             <Circle center={{lat:lat, lng:lng}} radius={parseFloat(range)}/>
-            {markers.map(marker => {
+            {/* Only shows friends inside the range selected by the user */}
+            {markers.map(marker => { 
                     if(distanceBetweenCoordinates(marker.props.position.lat, marker.props.position.lng) < parseFloat(range)) 
                         return marker;
+                    return null;
                 })
             }
-            {selected ? (<InfoWindow position={{lat:parseFloat(selected.split(",")[0]),lng: parseFloat(selected.split(",")[1])}} onCloseClick={()=>setSelected(null)} ><div>{friendsLocations.get(selected)}</div></InfoWindow> ) : null}
+            {/* Show friends information when click on its marker */}
+            {selected ? (<InfoWindow position={{lat:parseFloat(selected.split(",")[0]),lng: parseFloat(selected.split(",")[1])}} onCloseClick={()=>setSelected(null)} ><div>{friendsLocations.get(selected)[0]}</div></InfoWindow> ) : null}
         </GoogleMap>
     )
 }
