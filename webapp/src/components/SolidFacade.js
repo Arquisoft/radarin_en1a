3f53e0774720912d8883bdf9238ead1149432d32
CTPache <33907485+CTPache@users.noreply.github.com>
2021-04-28 11:10:56 +0200

    Initial code for the solid facade component

--- webapp/src/components/SolidFacade.js
+++ webapp/src/components/SolidFacade.js
@@ -28,4 +28,35 @@ class SolidFacade {
         return session;
     }
 
+    // Load the locations from solid and put them into the state
+    async loadStoredLocationFromSolid() {
+        let session = await this.getCurrentSession();
+
+        // If the file does not exist, is created
+        let fileUrl = session.webId.replace("profile/card#me", "radarin/stored_locations.json");
+        if (!(await fc.itemExists(fileUrl)))
+            await fc.postFile(fileUrl, new Blob());
+        // Until here
+
+        let url = session.webId.replace("profile/card#me", "radarin/stored_locations.json");
+        let radar = await fetch(url).then((x) => {
+            if (x.status === 200)  // if the file exists, return the text
+                return x.text()
+        });
+        if (radar === "")
+            return;
+
+        const locations = JSON.parse(radar);
+        return locations;
+    }
+
+    // Saves the locations into the solid profile
+    async saveStoredLocationToSolid(myLocations) {
+        let session = await this.getCurrentSession();
+        // If the file does not exist, it is created
+        let fileUrl = session.webId.replace("profile/card#me", "radarin/stored_locations.json");
+        let myJSON = JSON.stringify(myLocations);
+        await fc.postFile(fileUrl, new Blob([myJSON]));
+    }
+
 }
