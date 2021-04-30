1e428c5e87463af8f8dde7b12eaa88c6ba3f4287
Sergio García Díaz <sergady987@gmail.com>
2021-04-30 19:52:24 +0200

    Tests added on SideMenu.test.js

--- webapp/src/components/__test__/SideMenu.test.js
+++ webapp/src/components/__test__/SideMenu.test.js
@@ -6,4 +6,16 @@ it("Renders without crashing", () => {
     const div = document.createElement("div");
 
     ReactDOM.render(<SideMenu></SideMenu>, div);
-})
+
+});
+
+test("Shows the friends list", () => {
+    const div = document.createElement("div");
+    let friend = {
+        name : "Juan"
+    };
+    let friends = {friend}; 
+    ReactDOM.render(<SideMenu friends={friends} ></SideMenu>, div);
+    
+});
+
