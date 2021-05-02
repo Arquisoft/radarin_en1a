3e95acf4f0b72bc4c312baaa09582e0f6ec2a8a3
Sergio García Díaz <sergady987@gmail.com>
2021-05-02 18:06:32 +0200

    Mock added in SideMenu

--- webapp/src/components/__test__/SideMenu.test.js
+++ webapp/src/components/__test__/SideMenu.test.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import ReactDOM from 'react-dom';
 import SideMenu from '../SideMenu';
+
+// declare which API requests to mock
+const server = setupServer(
+  // capture "GET /greeting" requests
+  rest.get('https://uo264270.inrupt.net/radarin/stored_locations.json', (req, res, ctx) => {
+    // respond using a mocked JSON body
+    return res(ctx.json({"lat":43.368448,"lng":-5.8327040000000006,"name":"Ovi","photo":"./logo192.png"}))
+  })
+)
 
 it("Renders without crashing", () => {
     const div = document.createElement("div");
@@ -9,7 +20,7 @@ it("Renders without crashing", () => {
 
 });
 
-test("Shows the friends list", () => {
+test("Shows the locations list", () => {
     const div = document.createElement("div");
     let friend = {
         name : "Juan"
@@ -18,4 +29,6 @@ test("Shows the friends list", () => {
     ReactDOM.render(<SideMenu friends={friends} ></SideMenu>, div);
     
 });
+
+
 
