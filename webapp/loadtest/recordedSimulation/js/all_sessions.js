691b107d30ee6667177ee4a80c933d65b4c1a406
CTPache <33907485+CTPache@users.noreply.github.com>
2021-05-03 14:15:38 +0200

    load test script and result

---
+++ webapp/loadtest/recordedSimulation/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1620043978000,1],[1620043979000,1],[1620043980000,1],[1620043981000,1],[1620043982000,1],[1620043983000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
