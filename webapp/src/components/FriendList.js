d8a86e0fa8c1326a39ad20476a0515388e62276d
Diecaal <diecaal@gmail.com>
2021-05-11 19:25:12 +0200

    Loader friends permissions

--- webapp/src/components/FriendList.js
+++ webapp/src/components/FriendList.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 class FriendList extends React.Component {
 
+    changeFriendsPermission(friend){
+        let currentPermission = friend.permission;
+        friend.permissionsChanging = true;
+        this.props.handlePermission(friend);
+        var timer = setInterval(function(){
+            if(friend.permission !== currentPermission) {
+                friend.permissionsChanging = false;
+                clearInterval(timer);
+            }
+        }, 500);
+        setTimeout(function(){
+            clearInterval(timer);
+        }, 10000);;
+    }
+
     render() {
         let friendsList = this.props.friends;
         if (friendsList !== undefined)
@@ -17,18 +32,19 @@ class FriendList extends React.Component {
                             // Alt is a tag that's used when the image cannot be loaded,
                             // needed, if not warnings are sent
                             // Using target="_blank" without rel="noreferrer" is a security risk
-                            return <tr key= {"friend-tr-" + friend.pod}>
+                            return (<tr key= {"friend-tr-" + friend.pod}>
                             <th key= {"friend-th1-" + friend.pod} className="profile-pic"><img width="24px" height="24px" src={photo} alt="Load error" /></th>
                             <th key= {"friend-th2-" + friend.pod}><a id={"friend-" + friend.pod} target="_blank" rel="noreferrer" className="friendLink" href={friend.pod}>{friend.name}</a></th>
                             <th key= {"friend-th3-" + friend.pod}><button title="Enable/Disable friend's permissions" className="button-permission" id={"button-" + friend.pod} 
-                                        onClick={() => this.props.handlePermission(friend)}>{friend.permission?"✔️":"❌"}</button></th>
-                            </tr>
+                                onClick={() => this.changeFriendsPermission(friend)}> { friend.permission ? "✔️" : "❌" } </button></th> 
+                            {console.log(friend.permissionsChanging)} 
+                            {friend.permissionsChanging ? (friend.permission ? <th><div className="loader-mini-red"></div></th> : <th><div className="loader-mini-green"></div></th>) : null} </tr>) 
                         })}
                         </tbody>
-                    </table>
+                    </table> 
                 </div>)
         else return <div></div>;
     }
 }
 
-export default FriendList
+export default FriendList 
